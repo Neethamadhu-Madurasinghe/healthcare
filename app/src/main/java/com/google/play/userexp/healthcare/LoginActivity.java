90fc9c7cf1763ef75a1509b3036a81ef922b7454
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-02-15 22:40:19 +0530

    Create login activity UI

---
+++ app/src/main/java/com/google/play/userexp/healthcare/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.google.play.userexp.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText loginPassword, loginUsername;
+    Button loginButton;
+    TextView newUserReg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginUsername = findViewById(R.id.editTextLoginUsername);
+    }
+}
