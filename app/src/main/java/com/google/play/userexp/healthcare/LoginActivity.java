1e22cfd9072e18ee147d0039eaa2994d37324d4b
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-02-16 00:52:23 +0530

    Create Register activity

--- app/src/main/java/com/google/play/userexp/healthcare/LoginActivity.java
+++ app/src/main/java/com/google/play/userexp/healthcare/LoginActivity.java
@@ -2,16 +2,19 @@ package com.google.play.userexp.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText loginPassword, loginUsername;
     Button loginButton;
-    TextView newUserReg;
+    TextView newUserRegTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,31 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginUsername = findViewById(R.id.editTextLoginUsername);
+        loginPassword = findViewById(R.id.editTextLoginPassword);
+        loginButton = findViewById(R.id.loginButton);
+        newUserRegTextView = findViewById(R.id.regNewUser);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = loginUsername.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                if(username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please fill all the details", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        newUserRegTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Starting a new activity using an explicit intent
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/google/play/userexp/healthcare/RegisterActivity.java
@@ -0,0 +1,78 @@
+package com.google.play.userexp.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText registerUsername, registerEmail, registerPassword, registerConfirmPassword;
+    Button registerButton;
+    TextView existingUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        registerUsername = findViewById(R.id.editTextRegisterUsername);
+        registerEmail = findViewById(R.id.editTextRegisterEmail);
+        registerPassword = findViewById(R.id.editTextRegisterPassword);
+        registerConfirmPassword = findViewById(R.id.editTextRegisterConfirmPassword);
+        registerUsername = findViewById(R.id.editTextRegisterUsername);
+        registerButton = findViewById(R.id.registerButton);
+        existingUser = findViewById(R.id.textViewExistingUser);
+
+        existingUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = registerUsername.getText().toString();
+                String password = registerPassword.getText().toString();
+                String confirm = registerConfirmPassword.getText().toString();
+                String email = registerEmail.getText().toString();
+
+//                Validate
+                if(username.length() == 0 || email.length() == 0 || password.length() == 0 || confirm.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please fill all the details", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    if(password.compareTo(confirm) == 0) {
+                        if(isValid(password)) {
+                            Toast.makeText(getApplicationContext(), "Record added successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        }else {
+                            Toast.makeText(getApplicationContext(), "Password is too weak", Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Password and confirm password did not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+
+    public static boolean isValid(String password) {
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+}
