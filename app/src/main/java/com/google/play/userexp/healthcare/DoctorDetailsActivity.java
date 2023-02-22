545ee6bf51f578a047eb4fbdda8b7ee63617935f
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-02-22 23:15:09 +0530

    Add Doctor details activity

--- app/src/main/java/com/google/play/userexp/healthcare/DoctorDetailsActivity.java
+++ app/src/main/java/com/google/play/userexp/healthcare/DoctorDetailsActivity.java
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
     private String[][] doctor_details1 = {
@@ -20,6 +25,17 @@ public class DoctorDetailsActivity extends AppCompatActivity {
             {"Doctor Name: Chithral Somapala", "Hospital Address: Colombo", "Exp: 5yrs", "Mobile No: 071953221", "900"},
             {"Doctor Name: Ashok Panda", "Hospital Address: Pimpri", "Exp: 10yrs", "Mobile No: 0719532124", "700"},
     };
+
+    private String[][] doctor_details3 = {
+            {"Doctor Name: Chithral Sompala", "Hospital Address: Hambanthota", "Exp: 9yrs", "Mobile No: 0719532190", "700"},
+            {"Doctor Name: Priyantha Doe", "Hospital Address: kaluthara", "Exp: 5yrs", "Mobile No: 071953221", "900"},
+            {"Doctor Name: Ashok Panda", "Hospital Address: Pimpri", "Exp: 10yrs", "Mobile No: 0719532124", "700"},
+    };
+
+    private String[][] Doctor_details;
+    ArrayList list;
+    HashMap<String, String> item;
+    SimpleAdapter simpleAdapter;
 
     TextView titleView;
     Button backButton;
@@ -40,8 +56,11 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         if(title.compareTo("Family Physicians") == 0) {
             doctor_details = doctor_details1;
 
-        }else {
+        }else if(title.compareTo("Dieticians") == 0) {
             doctor_details = doctor_details2;
+
+        }else {
+            doctor_details = doctor_details;
         }
 
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +70,28 @@ public class DoctorDetailsActivity extends AppCompatActivity {
             }
         });
 
+        list = new ArrayList();
+        for(int i = 0; i < doctor_details.length; i++) {
+            item = new HashMap<String, String>();
+            item.put("line1", doctor_details[i][0]);
+            item.put("line2", doctor_details[i][1]);
+            item.put("line3", doctor_details[i][2]);
+            item.put("line4", doctor_details[i][3]);
+            item.put("line5","Consultant Fee:" + doctor_details[i][4]+"/=");
+            list.add(item);
+        }
 
+//         Simple adaptor sends data to a list in the view
+        simpleAdapter = new SimpleAdapter(
+                getApplicationContext(),
+                list,
+                R.layout.multi_lines,
+                new String[]{"line1", "line2", "line3", "line4", "line5"},
+                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e}
+                );
+
+
+        ListView listView = findViewById(R.id.listViewDoctorDetails);
+        listView.setAdapter(simpleAdapter);
     }
 }
